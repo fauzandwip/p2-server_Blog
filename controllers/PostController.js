d4d2ce99fca163e6d472d5ef19bc1e7bc7b8c4a8
fauzandwip <fauzan27dp@gmail.com>
2023-10-31 07:14:45 +0700

    add put and delete post routes add controller to put and delete post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -33,11 +33,52 @@ module.exports = class PostController {
       const post = await Post.findByPk(id);
 
       if (!post) {
-        res.status(404).json({ message: 'Error Not Found' });
+        res.status(404).json({ message: `Post with id ${id} is not found` });
         return
       }
       
       res.status(200).json(post);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+
+  static async updatePost(req, res) {
+    try {
+      const { id } = req.params;
+      const { title, content, imgUrl, CategoryId, UserId } = req.body;
+      const post = await Post.findByPk(id);
+
+      if (!post) {
+        res.status(404).json({ message: `Post with id ${id} is not found` });
+        return
+      }
+
+      const updatedPost = await post.update({ title, content, imgUrl, CategoryId, UserId });
+      res.status(200).json(updatedPost);
+    } catch (error) {
+      console.log(error);
+      if (error.name === 'SequelizeValidationError') {
+        res.status(400).json({ message: error.errors[0].message });
+        return
+      }
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+
+  static async destroyPost(req, res) {
+    try {
+      const { id } = req.params;
+      const post = await Post.findByPk(id);
+
+      if (!post) {
+        res.status(404).json({ message: `Post with id ${id} is not found` });
+        return
+      }
+
+      await post.destroy();
+      res.status(200).json({ message: `Post with id ${id} has been successfully deleted`});
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: 'Internal Server Error' });
--- routes/posts/index.js
+++ routes/posts/index.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get('/', PostController.getPosts);
 router.post('/', PostController.createPost);
 router.get('/:id', PostController.getPost);
+router.put('/:id', PostController.updatePost);
+router.delete('/:id', PostController.destroyPost);
 
 module.exports = router;
